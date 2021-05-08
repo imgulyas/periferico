ea7b69891f93c9ee397cf105cae00c07f64e59a3
Imre Gulyas <imgulyas@gmail.com>
2021-05-08 10:57:51 +0200

    add polysemy effect types and basic business logic idea

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,18 @@
 module Main (main) where
 
-import PerifericoHelper (someFunc)
-
+import PerifericoHelper
+import Polysemy
 
 main :: IO ()
-main = someFunc
+main = print "hello world"
+
+update :: [RawOrder] -> [RawOrder] -> [RawOrder]
+update imported _ = imported
+
+importDeduplicateUpdate :: Members '[RawOrderImporter, RawOrderExporter, RawOrderCache] m => Sem m ()
+importDeduplicateUpdate = do
+  imported <- importRawOrders
+  fromCache <- readCache
+  let updated = update imported fromCache
+  cacheRawOrders updated
+  exportRawOrders updated
--- src/PerifericoHelper.hs
+++ src/PerifericoHelper.hs
@@ -1,15 +1,54 @@
-{- |
-Copyright: (c) 2021 Imre Gulyas
-SPDX-License-Identifier: MIT
-Maintainer: Imre Gulyas <imgulyas@gmail.com>
+{-# LANGUAGE TemplateHaskell #-}
 
-Small util for Periferico
--}
-
+-- |
+-- Copyright: (c) 2021 Imre Gulyas
+-- SPDX-License-Identifier: MIT
+-- Maintainer: Imre Gulyas <imgulyas@gmail.com>
+--
+-- Small util for Periferico
 module PerifericoHelper
-       ( someFunc
-       ) where
+  ( RawOrder,
+    RawOrderImporter (..),
+    RawOrderExporter (..),
+    RawOrderCache (..),
+    importRawOrders,
+    exportRawOrders,
+    cacheRawOrders,
+    readCache,
+  )
+where
 
+import Polysemy
 
-someFunc :: IO ()
-someFunc = putStrLn ("someFunc" :: String)
+-- Order No	Order Date	Order Status	Delivery Details	Delivery Status	Billing Address	Shipping Address	Products	Total	Payment Method	Customer Note	kiszállítási határidő
+data RawOrder = RawOrder
+  { orderNo :: Text,
+    orderDate :: Text,
+    orderStatus :: Text,
+    deliveryDetails :: Text,
+    deliveryStatus :: Text,
+    billingAddress :: Text,
+    shippingAddress :: Text,
+    products :: Text,
+    total :: Text,
+    paymentMethod :: Text,
+    customerNote :: Text,
+    deliveryDeadline :: Text
+  }
+  deriving stock (Show, Eq)
+
+data RawOrderImporter m a where
+  ImportRawOrders :: RawOrderImporter m [RawOrder]
+
+makeSem ''RawOrderImporter
+
+data RawOrderExporter m a where
+  ExportRawOrders :: [RawOrder] -> RawOrderExporter m ()
+
+makeSem ''RawOrderExporter
+
+data RawOrderCache m a where
+  CacheRawOrders :: [RawOrder] -> RawOrderCache m ()
+  ReadCache :: RawOrderCache m [RawOrder]
+
+makeSem ''RawOrderCache
