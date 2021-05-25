290b8c21832716d988bed91586676baca01c1738
Imre Gulyas <imgulyas@gmail.com>
2021-05-25 22:13:02 +0200

    define some domain types

--- src/Domain.hs
+++ src/Domain.hs
@@ -21,3 +21,36 @@ data Order = Order
     _deliveryDeadline :: Text
   }
   deriving stock (Show, Eq)
+
+data OrderStatus
+
+-- data CreateOrderRequest
+
+data BillingInfo
+
+data PaymentMethod
+
+data ResponseMeta = RMeta
+  { _id :: Int,
+    _key :: Text,
+    _value :: Text
+  }
+
+data LineItem = LineItem
+  { _id :: Int,
+    _name :: Text,
+    _product_id :: Text,
+    _quantity :: Int
+  }
+
+data ResponseOrder = ROrder
+  { _id :: Text,
+    _status :: OrderStatus,
+    _total :: Int,
+    _billing :: BillingInfo,
+    _payment_method :: PaymentMethod,
+    _payment_method_title :: Text,
+    _customer_note :: Text,
+    _meta_data :: [ResponseMeta],
+    _line_items :: [LineItem]
+  }
